6ad6a34abb1bedf009bc1049f061bd4fffb0466e
enesttn <83828133+enesttn@users.noreply.github.com>
2022-11-08 11:30:29 +0300

    Add files via upload

---
+++ Table.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace GreatCustomers
+{
+    partial class Table
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtAdd = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ad = new System.Windows.Forms.Label();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btSil = new System.Windows.Forms.Button();
+            this.btEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.dtAdd);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtNo);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Ad);
+            this.groupBox1.Controls.Add(this.btUpdate);
+            this.groupBox1.Controls.Add(this.btSil);
+            this.groupBox1.Controls.Add(this.btEkle);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.txtCompany);
+            this.groupBox1.Controls.Add(this.txtAdres);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1077, 520);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(239, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Doğum Tarihi";
+            // 
+            // dtAdd
+            // 
+            this.dtAdd.Location = new System.Drawing.Point(315, 19);
+            this.dtAdd.Name = "dtAdd";
+            this.dtAdd.Size = new System.Drawing.Size(200, 20);
+            this.dtAdd.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Müşteri No";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(82, 18);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(113, 20);
+            this.txtNo.TabIndex = 34;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(239, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Şirket";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Adres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Telefon";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "E-Mail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Soyad";
+            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(19, 44);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(20, 13);
+            this.Ad.TabIndex = 26;
+            this.Ad.Text = "Ad";
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(629, 95);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(100, 35);
+            this.btUpdate.TabIndex = 25;
+            this.btUpdate.Text = "Güncelle";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // btSil
+            // 
+            this.btSil.Location = new System.Drawing.Point(629, 57);
+            this.btSil.Name = "btSil";
+            this.btSil.Size = new System.Drawing.Size(100, 35);
+            this.btSil.TabIndex = 24;
+            this.btSil.Text = "Sil";
+            this.btSil.UseVisualStyleBackColor = true;
+            this.btSil.Click += new System.EventHandler(this.btSil_Click);
+            // 
+            // btEkle
+            // 
+            this.btEkle.Location = new System.Drawing.Point(629, 19);
+            this.btEkle.Name = "btEkle";
+            this.btEkle.Size = new System.Drawing.Size(100, 35);
+            this.btEkle.TabIndex = 23;
+            this.btEkle.Text = "Ekle";
+            this.btEkle.UseVisualStyleBackColor = true;
+            this.btEkle.Click += new System.EventHandler(this.btEkle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(842, 370);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(315, 93);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(147, 20);
+            this.txtCompany.TabIndex = 21;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(315, 67);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(147, 20);
+            this.txtAdres.TabIndex = 20;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(315, 41);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(147, 20);
+            this.txtPhone.TabIndex = 19;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(82, 97);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(113, 20);
+            this.txtEmail.TabIndex = 18;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(82, 71);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(113, 20);
+            this.txtSoyad.TabIndex = 17;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(82, 45);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(113, 20);
+            this.txtAd.TabIndex = 16;
+            // 
+            // Table
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 544);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Table";
+            this.Text = "GreatCustomers";
+            this.Load += new System.EventHandler(this.Table_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Ad;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btSil;
+        private System.Windows.Forms.Button btEkle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtAdd;
+    }
+}
+
---
+++ Table.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace GreatCustomers
+{
+    public partial class Table : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        public Table()
+        {
+            InitializeComponent();
+        }
+
+        void GetTable()
+        {
+            conn = new SqlConnection(@"Server=DESKTOP-U2PVNK1\SQLEXPRESS;Database=HERMES;User Id=sa;Password=123456");
+            conn.Open();
+            da = new SqlDataAdapter("SELECT *FROM HermesTable53", conn);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            conn.Close();
+        }
+
+
+
+        private void Table_Load(object sender, EventArgs e)
+        {
+            GetTable();
+        }
+
+        private void Ad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtNo.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtAd.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtSoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtEmail.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtPhone.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtAdres.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            txtCompany.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+
+            dtAdd.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+        }
+
+        private void btEkle_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO HermesTable53 (first_name,last_name,email,phone,adress,company,bdate) VALUES (@first_name,@last_name,@email,@phone,@adress,@company,@bdate)";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@first_name", txtAd.Text);
+            cmd.Parameters.AddWithValue("@last_name", txtSoyad.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+            cmd.Parameters.AddWithValue("@adress", txtAdres.Text);
+            cmd.Parameters.AddWithValue("@company", txtCompany.Text);
+            cmd.Parameters.AddWithValue("@bdate", dtAdd.Value);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            GetTable();
+
+
+        }
+
+        private void btSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 0)
+            {
+                DataGridViewRow row = this.dataGridView1.SelectedRows[0];
+              var id=  row.Cells["ID"].Value.ToString();
+
+                string query = "DELETE FROM HermesTable53 WHERE ID= @ID";
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(id));
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                GetTable();
+
+            }
+
+
+
+
+
+
+
+        }
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+             string query = "UPDATE HermesTable53 SET first_name=@first_name, last_name=@last_name, email=@email, phone=@phone,adress=@adress,company=@company,bdate=@bdate WHERE ID=@ID";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(txtNo.Text));
+            cmd.Parameters.AddWithValue("@first_name", txtAd.Text);
+            cmd.Parameters.AddWithValue("@last_name", txtSoyad.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+            cmd.Parameters.AddWithValue("@adress", txtAdres.Text);
+            cmd.Parameters.AddWithValue("@company", txtCompany.Text);
+            cmd.Parameters.AddWithValue("@bdate", dtAdd.Value);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+           GetTable();
+          
+
+
+             /* if (dataGridView1.SelectedRows.Count != 0)
+             {
+                 DataGridViewRow row = this.dataGridView1.SelectedRows[0];
+                 var id = row.Cells["ID"].Value.ToString();
+
+                 string query = "UPDATE HermesTable53 SET first_name=@first_name, last_name=@last_name, email=@email, phone=@phone,adress=@adress,company=@company,bdate=@bdate WHERE ID=@ID";
+                 cmd = new SqlCommand(query, conn);
+                 cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(id));
+                 conn.Open();
+                 cmd.ExecuteNonQuery();
+                 conn.Close();
+                 GetTable();
+
+             }
+            
+            */
+
+
+
+
+
+
+        }
+    }
+}
